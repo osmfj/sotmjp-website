06733d15b4548c21ef3dc627eae138bca1dc8e95
Hiroshi Miura <miurahr@linux.com>
2014-09-21 09:07:04 +0900

    Added sotmjp/settings
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

---
+++ sotmjp/settings/production.py
@@ -0,0 +1,17 @@
+#:coding=utf-8:
+
+# Production settings
+from .server import *  # NOQA
+
+# From address for production
+DEFAULT_FROM_EMAIL = "State of the Map JP 2014 <no-reply@stateofthemap.jp>"
+SERVER_EMAIL = DEFAULT_FROM_EMAIL
+
+LOGGING['filters']['static_fields']['fields']['environment'] = 'production'
+
+ALLOWED_HOSTS = [
+    'stateofthemap.jp',
+]
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
