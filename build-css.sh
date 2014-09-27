c6c2cf259329fb31f05515c153e36048e988ef49
Hiroshi Miura <miurahr@linux.com>
2014-09-27 13:30:39 +0900

    yui compress: add missing --type option
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- build-css.sh
+++ build-css.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 BASEDIR=$(dirname $0)
 lessc ${BASEDIR}sotmjp/static/less/site.less \
- |  yui-compressor > ${BASEDIR}/sotmjp/static/css/site.css
+ |  yui-compressor --type css > ${BASEDIR}/sotmjp/static/css/site.css
