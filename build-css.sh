f35e9218764832000fde98cbb3bdea6f38e6821f
Hiroshi Miura <miurahr@linux.com>
2014-09-27 16:00:53 +0900

    lessc: build responsive-site.css
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- build-css.sh
+++ build-css.sh
@@ -2,3 +2,5 @@
 BASEDIR=$(dirname $0)
 lessc ${BASEDIR}/sotmjp/static/less/site.less \
  |  yui-compressor --type css > ${BASEDIR}/sotmjp/static/css/site.css
+lessc ${BASEDIR}/sotmjp/static/less/responsive-site.less \
+ |  yui-compressor --type css > ${BASEDIR}/sotmjp/static/css/responsive-site.css
