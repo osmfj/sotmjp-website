fb415da23dd93648e52b65520efbd7275880184f
Hiroshi Miura <miurahr@linux.com>
2014-09-27 13:32:35 +0900

    build-css: fix input path
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- build-css.sh
+++ build-css.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 BASEDIR=$(dirname $0)
-lessc ${BASEDIR}sotmjp/static/less/site.less \
+lessc ${BASEDIR}/sotmjp/static/less/site.less \
  |  yui-compressor --type css > ${BASEDIR}/sotmjp/static/css/site.css
