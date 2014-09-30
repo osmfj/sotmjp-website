ed4135eb34451c4856bc4aca243e878a40358021
Hiroshi Miura <miurahr@linux.com>
2014-10-01 08:52:26 +0900

    update production scripts
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- build-css.sh
+++ build-css.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 BASEDIR=$(dirname $0)
+mkdir -p ${BASEDIR}/sotmjp/static/css
 lessc ${BASEDIR}/sotmjp/static/less/site.less \
  |  yui-compressor --type css > ${BASEDIR}/sotmjp/static/css/site.css
 lessc ${BASEDIR}/sotmjp/static/less/responsive-site.less \
