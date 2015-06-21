0b11318ab28cf7649c320985493b4b16d724b28c
Hiroshi Miura <miurahr@linux.com>
2015-06-22 00:44:42 +0900

    enable more login method and drop yahoo.com
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- sotmjp/settings/base.py
+++ sotmjp/settings/base.py
@@ -241,17 +241,33 @@ ACCOUNT_EMAIL_CONFIRMATION_EMAIL = False
 
 ACCOUNT_DELETION_EXPUNGE_CALLBACK = 'sotmjp.account.callbacks.account_delete_expunge'
 
+TWITTER_CONSUMER_KEY         = ''
+TWITTER_CONSUMER_SECRET      = ''
+FACEBOOK_APP_ID              = ''
+FACEBOOK_API_SECRET          = ''
+LINKEDIN_CONSUMER_KEY        = ''
+LINKEDIN_CONSUMER_SECRET     = ''
+GOOGLE_CONSUMER_KEY          = ''
+GOOGLE_CONSUMER_SECRET       = ''
+GOOGLE_OAUTH2_CLIENT_ID      = ''
+GOOGLE_OAUTH2_CLIENT_SECRET  = ''
+
 AUTHENTICATION_BACKENDS = [
     # Permissions backends
     "symposion.teams.backends.TeamPermissionsBackend",
 
-     Social Auth Backends
-    "social_auth.backends.google.GoogleBackend",
-    "social_auth.backends.yahoo.YahooBackend",
-    "social_auth.backends.OpenIDBackend",
+    # Social Auth Backends
+    'social_auth.backends.twitter.TwitterBackend',
+    'social_auth.backends.facebook.FacebookBackend',
+    'social_auth.backends.google.GoogleOAuth2Backend',
+    'social_auth.backends.google.GoogleBackend',
+    'social_auth.backends.contrib.linkedin.LinkedinBackend',
+    'social_auth.backends.contrib.github.GithubBackend',
+    'social_auth.backends.OpenIDBackend',
 
     # Django User Accounts
     "account.auth_backends.EmailAuthenticationBackend",
+    'django.contrib.auth.backends.ModelBackend',
 ]
 
 SOCIAL_AUTH_PIPELINE = [
