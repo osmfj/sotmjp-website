09e0cd25846afb5d288e31a250621c9a5a7dea00
Hiroshi Miura <miurahr@linux.com>
2015-07-07 19:46:26 +0900

    fix Application labels arent unique, duplicates: proposals
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- sotmjp/proposals/apps.py
+++ sotmjp/proposals/apps.py
@@ -6,3 +6,4 @@ from django.utils.translation import ugettext_lazy as _
 class ProposalsConfig(AppConfig):
     name = "sotmjp.proposals"
     verbose_name = _("Proposals")
+    label = "sotmjp_proposals"
